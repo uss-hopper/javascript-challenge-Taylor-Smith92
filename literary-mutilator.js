86663174c686b36fb13f63ef77d4ee3425666797
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-21 16:20:17 -0700

    failed to add button

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -1,7 +1,23 @@
-let target = document.getElementById("target");
+/**let target = document.getElementById("target");
 
 target.fetch("https://jsonplaceholder.typicode.com/posts/1", {
 	method: "GET",
 } )
 	.then(response => response.json())
 	.then(json => console.log(json))
+**/
+
+let tacoWords = document.getElementById("taco-words");
+const tacoButton = document.getElementById("taco-btn");
+let words = tacoWords.innerHTML;
+
+let onBtnClick = function(words) {
+	for (var i=0; i<words.length, i++) {
+		words[i].textContent = words[i].textContent.replace("taco", "burrito");
+	}
+};
+
+tacoButton.addEventListener("click", onBtnClick);
+
+
+
