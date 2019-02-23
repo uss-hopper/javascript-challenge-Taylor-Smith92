f5775f39e06f69d858d03a5da21603e453ba975f
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-23 16:03:59 -0700

    added map with replace function, inner function returns expected, global value is undefined

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -7,17 +7,22 @@ target.fetch("https://jsonplaceholder.typicode.com/posts/1", {
 	.then(json => console.log(json))
 **/
 
-let tacoWords = document.getElementById("taco-words");
-const tacoButton = document.getElementById("taco-btn");
-let words = tacoWords.innerHTML;
 
-let onBtnClick = function(words) {
-	for (var i=0; i<words.length, i++) {
-		words[i].textContent = words[i].textContent.replace("taco", "burrito");
-	}
-};
 
-tacoButton.addEventListener("click", onBtnClick);
+
+function lookInScriptJsLine11(split) {
+	let tacoWords = document.getElementById("taco-words");
+	let words = tacoWords.innerHTML;
+	let splitting = words.split(" ");
+	let burritoReplace = splitting.map((index) => {
+		index = index.replace("taco", "burrito");
+		console.log(index);
+	});
+
+
+	let target = document.getElementById("taco-words");
+	target.innerHTML = burritoReplace;
+}
 
 
 
